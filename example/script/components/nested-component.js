06f1c6b7e7890baa317d90d575c91b55d0e86a32
Diogo Ferreira Fernandes <diogo.fernandes.external@ynap.com>
2022-08-31 21:40:40 +0100

    Update nested-component

--- example/script/components/nested-component.js
+++ example/script/components/nested-component.js
@@ -4,7 +4,7 @@ CreateComponent(({ html }) => {
   return html`
     <p>
       Hello
-      <nesting-component who="world"></nesting-component>
+      <nesting-component who="world"></nesting-component>!
     </p>
     <p><a href="https://github.com/imaginamundo/webcomponents/blob/main/example/script/components/nested-component.js">Repository link</a></p>
   `;
